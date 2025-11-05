50d94d87cd0c38daee482ebd3a440cdda4881482
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-11-05 15:32:24 +0000

    rota bemvindo

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 @app.route('/info')
 def info():
-    modulo = "Flask"
+    modulo = "CSS"
     aula = "1"
     return "<h1>pagina info</h1>"
 
 @app.route('/')
 def index():
     return f"<h1>Modulo: {modulo}, Aula: {aula}</h1>"
+
+@app.route('/bemvindo/usuario')
+def bemvindo(usuario):
+    return f"<h1>Bem vindo{usuario.capitalize()} </h1>"

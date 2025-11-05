6233af3929df78a80a07c79bd5c7dc22605edf89
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-11-05 16:06:38 +0000

    rota home

--- app.py
+++ app.py
@@ -20,3 +20,11 @@ from flask import Flask, redirect
 def home(): 
 
   return redirect("/")
+
+from flask import Flask, redirect, render_template
+
+@app.get("/sobre") 
+
+def sobre(): 
+
+   return render_template("sobre.html")

a34846cff2132cd891b44807a225301d086a6efe
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-11-05 22:10:43 +0000

    Corrigindo rota bemvindo e adicionando rota sobre

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
@@ -9,14 +9,10 @@ def info():
     aula = "1"
     return f"<h1>Modulo: {modulo}, Aula: {aula}</h1>"
 
-@app.route('/bemvindo/usuario')
+@app.route('/bemvindo/<usuario>')
 def bemvindo(usuario):
     return f"<h1>Bem vindo{usuario.capitalize()} </h1>"
 
-from flask import Flask, redirect, render_template
-
 @app.get("/sobre") 
-
 def sobre(): 
-
-   return render_template("sobre.html")
+    return render_template("sobre.html")

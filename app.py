ec5bcaa5ea7db537bfee46b0684764619538560f
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-11-05 16:01:05 +0000

    rota home

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/info')
 def info():
     modulo = "CSS"
     aula = "1"
-    return "<h1>pagina info</h1>"
-
-@app.route('/')
-def index():
     return f"<h1>Modulo: {modulo}, Aula: {aula}</h1>"
 
 @app.route('/bemvindo/usuario')
 def bemvindo(usuario):
     return f"<h1>Bem vindo{usuario.capitalize()} </h1>"
+
+from flask import Flask, redirect
+
+@app.get("/home") 
+
+def home(): 
+
+  return redirect("/")
